424ef2463600e5d41f1a4f28ce6707dc47c7f168
Joni <74.boyko@gmail.com>
2022-12-12 18:29:35 +0200

    add use state to all sign up fields

--- src/components/sign-up/sign-up.js
+++ src/components/sign-up/sign-up.js
@@ -5,27 +5,48 @@ import "./sign-up.css"
 import {Link} from "react-router-dom";
 
 function SignUp(props) {
-  const [first, changeFirst] = React.useState("")
+  const [user, changeUser] = React.useState({
+    firstName: "",
+    lastName: "",
+    email : "",
+    password: ""
+  })
 
   const onSignUp = (e) => {
     e.preventDefault();
-    console.log('hello',first)
   }
-  const onChangeFirst = (e) => {
-    changeFirst(e.target.value)
-}
-
+  const onChangeFirstName = (e) => {
+    const newFirst = Object.assign({},user, {firstName: e.target.value})
+    changeUser(newFirst)
+  }
+  console.log()
+  const onChangeLastName = (e) => {
+    const newLast = Object.assign({},user, {lastName: e.target.value})
+    changeUser(newLast)
+  }
+  const onChangeEmail = (e) => {
+    changeUser( {
+      ...user,
+      email: e.target.value
+    })
+  }
+  const onChangePassword = (e) => {
+    changeUser( {
+      ...user,
+      password: e.target.value
+    })
+  }
   return (
     <div className="sign-up">
       <form onSubmit={onSignUp} className="sign-up__form">
         <h2>Sign up</h2>
-        <TextField  value={first} onChange={onChangeFirst} className="sign-up__first-name"  id="standard-basic" label="First name" variant="standard" fullWidth />
-        <label htmlFor="Fname"></label><br/>
-        <TextField  className="sign-up__last-name" id="standard-basic" label="Last name" variant="standard" fullWidth  />
+        <TextField  value={user.firstName} onChange={onChangeFirstName} className="sign-up__first-name"  id="standard-basic" label="First name" variant="standard" fullWidth />
+        <label htmlFor="fName"></label><br/>
+        <TextField value={user.lastName} onChange={onChangeLastName} className="sign-up__last-name" id="standard-basic" label="Last name" variant="standard" fullWidth  />
+        <label htmlFor="lName"></label><br/>
+        <TextField  value={user.email} onChange={onChangeEmail} className="sign-up__email" id="standard-basic" type="email" label="Email" variant="standard"  fullWidth />
         <label htmlFor="email"></label><br/>
-        <TextField className="sign-up__email" id="standard-basic" type="email" label="Email" variant="standard"  fullWidth />
-        <label htmlFor="email"></label><br/>
-        <TextField className="sign-up__password" id="standard-basic" type="password" label="Password" variant="standard" fullWidth />
+        <TextField value={user.password} onChange={onChangePassword} className="sign-up__password" id="standard-basic" type="password" label="Password" variant="standard" fullWidth />
         <br/>
         <p className="sign-up__advice">Already have an account, then <Link to="/">log in</Link></p>
         <br/>
@@ -37,4 +58,6 @@ function SignUp(props) {
   );
 }
 
+
 export default SignUp;
+
