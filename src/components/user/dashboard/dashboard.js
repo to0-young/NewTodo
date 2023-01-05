b9e7aa076bb49a474fe67260b2c3e0e5140eb019
to0-young <74.boyko@gmail.com>
2023-01-05 22:09:22 +0200

    add dashboard table

--- src/components/user/dashboard/dashboard.js
+++ src/components/user/dashboard/dashboard.js
@@ -1,9 +1,58 @@
-import React from 'react';
-function Dashboard(props) {
+import * as React from 'react';
+import './dashboard.css'
+
+
+export default function DataTable() {
+  const myRows = [
+    { title: 'Посрати', desc: 'Піти посрати в туалет', priority: 1, dueDate: new Date().toDateString() },
+    { title: 'Посрати', desc: 'Піти посрати в туалет', priority: 1, dueDate: new Date().toDateString() },
+  ]
+
+  const myRows2 = [
+    { title: 'Посрати', desc: 'Піти посрати в туалет', priority: 1, dueDate: new Date().toDateString() },
+    { title: 'Посрати', desc: 'Піти посрати в туалет', priority: 1, dueDate: new Date().toDateString() },
+  ]
   return (
-    <>
-      <div>Dashboard</div>
-    </>
-);
+    <div className='dashboard'>
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Priority</th>
+          <th>Due date</th>
+        </tr>
+
+        {myRows2.map((row, index) => {
+          return (
+            <tr key={index}>
+              <td>{row.title}</td>
+              <td>{row.desc}</td>
+              <td>{row.priority}</td>
+              <td>{row.dueDate}</td>
+            </tr>
+          )
+        })}
+      </table>
+
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Priority</th>
+          <th>Due date</th>
+        </tr>
+
+        {myRows.map((row, index) => {
+          return (
+            <tr key={index}>
+              <td>{row.title}</td>
+              <td>{row.desc}</td>
+              <td>{row.priority}</td>
+              <td>{row.dueDate}</td>
+            </tr>
+          )
+        })}
+      </table>
+    </div>
+  )
 }
-export default Dashboard;
