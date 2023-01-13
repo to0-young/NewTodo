52f43f697b2ff8d96f5ce545888244a339635ebe
to0-young <74.boyko@gmail.com>
2023-01-13 16:50:15 +0200

    add request task validations

--- src/components/user/new-task/new-task.js
+++ src/components/user/new-task/new-task.js
@@ -7,54 +7,151 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import Button from "@mui/material/Button";
 
 export default function NewTask() {
-  return (
-    <div className="new-task">
-      <form className="new-task__form">
-        <br/>
-        <h2>New Task</h2>
-        <br/>
+  const [task, changeTask] = React.useState( {
+    title: "",
+    description: "",
+    priority: 1,
+    dueDate: new Date(),
+  })
 
-        <TextField
-          label="Title"
-          variant="standard"
-          fullWidth
-        />
 
-        <br/>
-        <br/>
+  const [error, changeError] = React.useState({
+    title: "",
+    description: "",
+    priority: ""
+  })
 
-        <TextField
-          label="Description"
-          variant="standard"
-          fullWidth
-        />
+const onValidation = () => {
+  let valid = true
+  const appError = {
+    title: "",
+    priority: ""
+  }
 
-        <br/>
-        <br/>
+  if (task.title.length < 3 ) {
+    valid = false
+    appError.title = "Sorry your title is too short"
+  }
+  if (task.priority.length < 1 ) {
+    valid = false
+    appError.priority = "Sorry your priority is too short"
+  }
+  if(!valid) {
+    changeError(appError)
+  }
+  return valid
+  }
 
-        <TextField
-          type="number"
-          label="Priority"
-          variant="standard"
-          fullWidth
-        />
+  const onNewTask = async (e) => {
+    e.preventDefault()
+    if (onValidation()) {
+      await postTask()
+    }
+  }
 
-        <br/>
-        <br/>
+  const onChangeTitle = (e) => {
+    changeTask({
+      ...task,
+      title: e.target.value
+    })
+  }
 
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateTimePicker
-            label="Due date"
-            onChange={() => {}}
+  const onChangeDescription = (e) => {
+    changeTask({
+      ...task,
+      description: e.target.value
+    })
+  }
+
+  const onChangePriority = (e) => {
+    changeTask({
+      ...task,
+      priority: e.target.value
+    })
+  }
+
+  const onChangeDate = (value) => {
+    changeTask({
+      ...task,
+      dueDate: value
+    })
+  }
+
+  const postTask = async () => {
+    const res = await fetch('http://localhost:3000/api/v1/tasks', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        title: task.title,
+        description: task.description,
+        priority: task.priority,
+        due_date: task.dueDate
+      })
+    })
+    const json = await res.json()
+    console.log(json)
+  }
+
+    return (
+      <div className="new-task">
+        <form onSubmit={onNewTask} className="new-task__form">
+          <br/>
+          <h2>New Task</h2>
+          <br/>
+          <TextField
+            value={task.title}
+            helperText={error.title}
+            error={"" !== error.title}
+            helperText={task.title}
+            onChange={onChangeTitle}
+            label="Title"
+            variant="standard"
             fullWidth
-            renderInput={(params) => <TextField {...params} />}
           />
-        </LocalizationProvider>
 
-        <br/>
-        <Button onClick={NewTask}  variant="contained" color="info">Choice</Button>
-        <br/>
-      </form>
-    </div>
-  );
+          <br/>
+          <br/>
+
+          <TextField
+            value={task.description}
+            onChange={onChangeDescription}
+            label="Description"
+            variant="standard"
+            fullWidth
+          />
+
+          <br/>
+          <br/>
+
+          <TextField
+            value={task.priority}
+            error={"" !== error.priority}
+            helperText={error.priority}
+            onChange={onChangePriority}
+            label="Priority"
+            type={'number'}
+            variant="standard"
+            fullWidth
+          />
+
+          <br/>
+          <br/>
+
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateTimePicker
+              label="Due date"
+              onChange={onChangeDate}
+              value={task.dueDate}
+              fullWidth
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+
+          <br/>
+          <Button type={"submit"}  variant="contained" color="info">Create</Button>
+          <br/>
+        </form>
+      </div>
+    );
 }
