8808f78ee63d2fef0f2037fea9237f88d30fabf0
Yevhen <to0-young>
2023-04-10 13:14:11 +0300

    add new upload & spinner

--- src/components/reusable/spinner.js
+++ src/components/reusable/spinner.js
@@ -2,11 +2,12 @@ import Stack from '@mui/material/Stack'
 import CircularProgress from '@mui/material/CircularProgress'
 import React from 'react'
 
-export default function Spinner() {
+export default function Spinner(props) {
+  const { color, size } = props
   return (
     <div className='spinner'>
       <Stack sx={{ color: 'grey.500', width: '100%' }} spacing={2}>
-        {<CircularProgress style={{ margin: 'auto' }} color='inherit' />}
+        {<CircularProgress style={{ margin: 'auto', width: size, height: size }} color={color} />}
       </Stack>
     </div>
   )
